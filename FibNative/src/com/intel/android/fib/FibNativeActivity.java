8ee5344540bdfddeb6413034230ca25603193d9a
Marko Gargenta <marko@marakana.com>
2012-04-23 04:14:05 -0700

    Implemented iniail UI

--- FibNative/src/com/intel/android/fib/FibNativeActivity.java
+++ FibNative/src/com/intel/android/fib/FibNativeActivity.java
@@ -2,12 +2,23 @@ package com.intel.android.fib;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FibNativeActivity extends Activity {
+	EditText input;
+	TextView output;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    /** Called when button is pressed. */
+    public void onClick(View v) {
+    	
     }
 }
