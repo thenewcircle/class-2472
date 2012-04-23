56bbf568c4f986ffede211014a3640dffd5237f6
Marko Gargenta <marko@marakana.com>
2012-04-23 03:16:32 -0700

    Implemented initial FibLib.java

---
+++ FibNative/src/com/intel/android/hello/FibLib.java
@@ -0,0 +1,17 @@
+package com.intel.android.hello;
+
+public class FibLib {
+
+	/** Java version of Fibonacci algorithm.
+	 * @param n Input value
+	 * @return Fibonacci of n
+	 */
+	public static long fibJ(long n) {
+		if(n==0) return 0;
+		if(n==1) return 1;
+		return fibJ(n-1)+fibJ(n-2);
+	}
+	
+	/** Declaration of native Fibonacci function. */
+	public static native long fibN(long n);
+}
