c74d1db81d62e0ddf8319ea371224669a25164db
Marko Gargenta <marko@marakana.com>
2012-04-24 21:01:14 -0700

    Added LogManager

--- LogClient/src/com/intel/android/logclient/LogClientActivity.java
+++ LogClient/src/com/intel/android/logclient/LogClientActivity.java
@@ -1,43 +1,24 @@
 package com.intel.android.logclient;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.view.View;
 
-import com.intel.android.logcommon.ILogService;
 import com.intel.android.loglib.LogLib;
 
 public class LogClientActivity extends Activity {
-	ILogService logService;
+	LogManager logManager;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
-		bindService(new Intent("com.intel.android.logcommon.ILogService"),
-				new ServiceConnection() {
-					@Override
-					public void onServiceConnected(ComponentName name,
-							IBinder binder) {
-						logService = ILogService.Stub.asInterface(binder);
-					}
-
-					@Override
-					public void onServiceDisconnected(ComponentName name) {
-						logService = null;
-					}
-				}, Context.BIND_AUTO_CREATE);
+		
+		logManager = new LogManager(this);
 	}
 
-	public void onClick(View v) throws RemoteException {
-		logService.log(LogLib.DEBUG, "LogLib", "onCreated");
+	public void onClick(View v) {
+		logManager.log(LogLib.DEBUG, "LogLib", "onCreated");
 	}
 }
---
+++ LogClient/src/com/intel/android/logclient/LogManager.java
@@ -0,0 +1,41 @@
+package com.intel.android.logclient;
+
+import com.intel.android.logcommon.ILogService;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+
+public class LogManager {
+	private ILogService logService;
+
+	public LogManager(Context context) {
+		context.bindService(new Intent(
+				"com.intel.android.logcommon.ILogService"),
+				new ServiceConnection() {
+					@Override
+					public void onServiceConnected(ComponentName name,
+							IBinder binder) {
+						logService = ILogService.Stub.asInterface(binder);
+					}
+
+					@Override
+					public void onServiceDisconnected(ComponentName name) {
+						logService = null;
+					}
+				}, Context.BIND_AUTO_CREATE);
+	}
+
+	// --- Proxy calls to the remote service
+
+	public void log(int priority, String tag, String message) {
+		try {
+			logService.log(priority, tag, message);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+}
